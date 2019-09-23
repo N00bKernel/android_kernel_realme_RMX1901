b8ba42298a65347d071f99d45bc5bed648bd6cfb
thanasis <thanasis>
2019-09-23 08:20:49 +0300

    sched/fair: lower sched latency

--- kernel/sched/fair.c
+++ kernel/sched/fair.c
@@ -46,9 +46,11 @@
  *
  * (to see the precise effective timeslice length of your workload,
  *  run vmstat and monitor the context-switches (cs) field)
+ *
+ * (default: 6ms * (1 + ilog(ncpus)), units: nanoseconds)
  */
-unsigned int sysctl_sched_latency = 6000000ULL;
-unsigned int normalized_sysctl_sched_latency = 6000000ULL;
+unsigned int sysctl_sched_latency			= 5000000ULL;
+unsigned int normalized_sysctl_sched_latency		= 5000000ULL;
 
 /*
  * The initial- and re-scaling of tunables is configurable
